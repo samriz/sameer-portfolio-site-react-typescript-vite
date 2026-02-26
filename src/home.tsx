8bcc9d2eefd529cb65b2edf193f3a3b3ec4d6be1
samriz <samriz@users.noreply.github.com>
2026-02-26 06:30:54 -0600

    Changed color of page links for better color contrast and replaced instances of <HashLink> to <Link> in home page.

--- src/home.tsx
+++ src/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { HashLink } from 'react-router-hash-link';
+//import { HashLink } from 'react-router-hash-link';
 import PageTemplate from "./pagetemplate";
 import Resume from "./misc/SameerRizviResume.pdf";
 import { ExternalUrlArrow } from './components/icons';
@@ -18,9 +18,9 @@ export default class Home extends React.Component
                     <figure>
                         <a href="https://www.linkedin.com/in/sameer-r-bb788375/" target="_blank" rel="noreferrer"><img src={LinkedInPicture} id="profilepic" className="center" title="Sameer Rizvi" alt="Sameer Rizvi" loading="lazy"/></a>
                         <figcaption>
-                            <HashLink smooth to="/contact" className="textlink" title="The Contact Page">Contact Me</HashLink>
+                            <Link to="/contact" className="pageLink" title="The Contact Page">Contact Me</Link>
                             <br/>
-                            <a href={"https://www.linkedin.com/in/sameer-r-bb788375/"} target={"_blank"} rel="noreferrer" title="My LinkedIn Page">LinkedIn <ExternalUrlArrow/></a>
+                            <a href={"https://www.linkedin.com/in/sameer-r-bb788375/"} className="pageLink" target={"_blank"} rel="noreferrer" title="My LinkedIn Page">LinkedIn <ExternalUrlArrow/></a>
                         </figcaption>
                     </figure>
                 </>
@@ -37,16 +37,16 @@ export default class Home extends React.Component
                 <p>Welcome to my official Web Portfolio.</p>
                 <p>Here, you'll find information pertaining to my academic and professional experiences. This includes, but is not limited to, the following:</p>
                 <ul>
-                    <li>My <Link to={Resume} title="Link to my Resume" target={"_blank"} rel={"author"}>resume <ExternalUrlArrow/></Link>.</li>
+                    <li>My <Link to={Resume} className="pageLink" title="Link to my Resume" target={"_blank"} rel={"author"}>resume <ExternalUrlArrow/></Link>.</li>
                     <li>An 
-                        <HashLink smooth to={"/about"} title="The About Page" rel="author"> about</HashLink> section, detailing my academic and professional history.
+                        <Link to="/about" className="pageLink" title="The About Page" rel="author"> about</Link> section, detailing my academic and professional history.
                     </li>
-                    <li>My <a href={"https://github.com/samriz"} target={"_blank"} title="My GitHub Page" rel={"noopener noreferrer"}>GitHub page <ExternalUrlArrow/></a>.</li>
-                    <li>A way to <HashLink smooth to={"/contact"} title="The Contact Page" rel={"author"}>contact</HashLink> me.</li>
+                    <li>My <a href={"https://github.com/samriz"} className="pageLink" target={"_blank"} title="My GitHub Page" rel={"noopener noreferrer"}>GitHub page <ExternalUrlArrow/></a>.</li>
+                    <li>A way to <Link to="/contact" className="pageLink" title="The Contact Page" rel={"author"}>contact</Link> me.</li>
                     {/* <li>
                         <HashLink smooth to={"/#summaryProfessionalSamples"} onClick={this.openProfessionalSamples}>Links</HashLink> to some of my professional work.
                     </li> */}
-                    <li>My code for this site can be found on <a href="https://github.com/samriz/sameer-portfolio-site-react-typescript-vite" target="_blank" title="Site's GitHub Repo" rel="noopener noreferrer">GitHub <ExternalUrlArrow/></a>.</li>
+                    <li>My code for this site can be found on <a href="https://github.com/samriz/sameer-portfolio-site-react-typescript-vite" className="pageLink" target="_blank" title="Site's GitHub Repo" rel="noopener noreferrer">GitHub <ExternalUrlArrow/></a>.</li>
                 </ul>
             </div>
         );
