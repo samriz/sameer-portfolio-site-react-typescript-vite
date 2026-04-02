75334ac772aee9b64fb8c8b6aec1ec7984ccc339
samriz <samriz@users.noreply.github.com>
2026-04-02 17:51:40 -0500

    Added indentations and newlines to improve readability of SocialMediaBanner code.

--- src/components/socialmediabanner.tsx
+++ src/components/socialmediabanner.tsx
@@ -5,11 +5,13 @@ export default function SocialMediaBanner()
     return (
         <div id="socialMediaBannerDiv">
             <a href="https://www.linkedin.com/in/sameer-r-bb788375/" target="_blank" rel="noopener noreferrer" title="My LinkedIn Page">
-            <LinkedInIcon/>
+                <LinkedInIcon/>
             </a>
+
             &nbsp;
+            
             <a href="https://github.com/samriz" target="_blank" rel="noopener noreferrer" title="My GitHub Page">
-            <GitHubIcon/>
+                <GitHubIcon/>
             </a>
         </div>
     );
