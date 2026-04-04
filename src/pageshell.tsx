a90dd777889107101abdae068d322be84bc4fae8
samriz <samriz@users.noreply.github.com>
2026-04-03 22:44:08 -0500

    Converted "PageShell" from a class component to a function component.

--- src/pageshell.tsx
+++ src/pageshell.tsx
@@ -1,56 +1,30 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Menu from "./components/menu";
 import MobileMenu from './components/mobilemenu';
 import Footer from "./components/footer";
 import "./styles/index.css";
-
-interface IndexState
-{
-    viewportWidth: number;
-}
 
 interface PageShellProps
 {
     pages: React.ReactNode[];
 }
 
-export default class PageShell extends React.Component<PageShellProps, IndexState>
+export default function PageShell({pages}: PageShellProps)
 {
-    constructor(props)
-    {
-        super(props);
-        window.visualViewport? this.state = { viewportWidth: window.visualViewport.width } : this.state = { viewportWidth: 0 };
-    }
+    const [viewportWidth, setViewportWidth] = useState(window.visualViewport? window.visualViewport.width : 0);
 
-    render()
-    {
-        if(this.state.viewportWidth > 640)
-        {
-            return (
-                <>
-                    <Menu/>
-                    {this.props.pages}
-                    <Footer/>               
-                </>
-            );
-        }
-        else
-        {
-            return (
-                <>
-                    <MobileMenu/>
-                    {this.props.pages}
-                    <Footer/>               
-                </>
-            );
-        }
-    }
-
-    componentDidMount()
-    {
+    useEffect(() => {
         window.addEventListener("resize", () => {
-            //console.log("resizing");
-            window.visualViewport? this.setState({viewportWidth: window.visualViewport.width}) : this.setState({viewportWidth: 0});
+            //console.log("resizing from page shell");
+            setViewportWidth(window.visualViewport? window.visualViewport.width : 0);
         });
-    }
+    }, []); //The empty array means that this effect will only run once when the component is mounted(added to DOM). Equivalent to componentDidMount() in class components.
+    
+    return (
+        <>
+            {viewportWidth > 640? <Menu/> : <MobileMenu/>}
+            {pages}
+            <Footer/>
+        </>
+    );
 }
