ef4c44c452a7684d397ac25703b3f002406cc9ac
samriz <samriz@users.noreply.github.com>
2026-04-07 17:38:05 -0500

    Removed commented out code in buttons.tsx.

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -12,18 +12,3 @@ export function DownloadButton({file, buttonText, downloadedFileName}: DownloadB
         <button className={"leftPad roundedCorners"}><a href={file} download={downloadedFileName}>{buttonText}</a></button>
     );
 }
-
-/* export default class DownloadButton extends React.Component<DownloadButtonProps>
-{
-    render()
-    {
-        return (
-            <button type="button" onClick={async () => await this.getResource(this.props.file)}>{this.props.buttonText}</button>
-        );
-    }
-    
-    getResource = async (resource: any) => {
-        const response = await fetch(resource);
-        const resourceBlob = await response.blob();
-    }
-} */
